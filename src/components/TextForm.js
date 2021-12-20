3f9080d6c14c80b3fb3f45d124aed779f8976fc6
GopaladPRO <gopalad.pro1994@gmail.com>
2021-12-20 15:33:14 +0530

    Optimised few codes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,9 +21,7 @@ export default function TextForm(props) {
         props.showAlert("Text reversed.","success");
     }
     const handleCopy = ()=>{
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert("Text copied.","success");
     }
     const handleExtraSpaces = ()=>{
@@ -39,17 +37,17 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{backgroundColor:props.taMode,color:props.mode==='light'?'black':'white'}}></textarea>
             </div>
-            <button className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleUpperCaseClick} >Convert to UpperCase</button>
-            <button className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleLowerCaseClick} >Convert to LowerCase</button>
-            <button className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleReverseClick} >Reverse text</button>
-            <button className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleCopy} >Copy text</button>
-            <button className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleExtraSpaces} >Remove extra spaces</button>
-            <button className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleClearClick} >Clear text</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleUpperCaseClick} >Convert to UpperCase</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleLowerCaseClick} >Convert to LowerCase</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleReverseClick} >Reverse text</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleCopy} >Copy text</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleExtraSpaces} >Remove extra spaces</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} bg-${props.mode} mx-2 my-2`} onClick={handleClearClick} >Clear text</button>
         </div>
         <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
             <h2>Your text Summary</h2>
-            <p>{text.split(' ').filter(n=>n).length} words, {text.length} characters</p>
-            <p>{text.split(' ').filter(n=>n).length * 0.008} Minutes read.</p>
+            <p>{text.split(/\s+/).filter(n=>n).length} words, {text.length} characters</p>
+            <p>{text.split(/\s+/).filter(n=>n).length * 0.008} Minutes read.</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something to preview it here."}</p>
         </div>
